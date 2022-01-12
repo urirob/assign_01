783e675945795c9b42622f4fdbf5659a0d65e2a1
urirob <91110790+urirob@users.noreply.github.com>
2022-01-12 19:48:46 +0530

    Add files via upload

---
+++ output_assgn_01.py
@@ -0,0 +1,39 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:\Users\DELL\OneDrive\Desktop\assgn_01.py =============
+enter first number : 
+12
+enter second number : 
+343
+enter third number : 
+76
+('average of given numbers is: ', 143.66666666666666)
+enter the number of depedents : 
+3
+enter the gross income : 
+1000000
+196200.0
+enter SID : 
+20103105
+enter name :
+Tarundeep Singh
+enter your gender :
+M
+enter your course name :
+Intro to Computing
+enter the CGPA :9
+[20103105, 'Tarundeep Singh', 'M', 'Intro to Computing', 9.0]
+enter marks of 1st student :
+90
+enter marks of 2nd student :
+56
+enter marks of 3rd student :
+78
+enter marks of 4th student :
+56
+enter marks of 5th student :
+82
+[56, 56, 78, 82, 90]
+['red', 'green', 'white', 'purple', 'purple', 'yellow']
+>>> 
